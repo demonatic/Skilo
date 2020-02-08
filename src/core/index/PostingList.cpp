58d2f64b3addb6833baa3f979404ae01839d0b68
demonatic <793626853@qq.com>
2020-02-08 22:10:25 +0800

    add offset get set to postingList

--- src/core/index/PostingList.cpp
+++ src/core/index/PostingList.cpp
@@ -18,6 +18,11 @@ void PostingList::add_doc(const uint32_t seq_id,const std::vector<uint32_t> &off
      uint32_t term_freq=static_cast<uint32_t>(offsets.size());
     _doc_ids.append(seq_id);
     _doc_term_freqs.append(term_freq);
+    uint32_t curr_offset_index=_offsets.length();
+    _offset_index.append(curr_offset_index);
+    for(uint32_t off:offsets){
+        _offsets.append(off);
+    }
 }
 
 uint32_t PostingList::get_doc_id(const uint32_t index) const
@@ -29,6 +34,18 @@ uint32_t PostingList::get_doc_tf(const uint32_t doc_id) const
 {
     uint32_t index=_doc_ids.index_of(doc_id);
     return _doc_term_freqs.at(index);
+}
+
+std::vector<uint32_t> PostingList::get_offsets(const uint32_t doc_id) const
+{
+    uint32_t pos=_doc_ids.index_of(doc_id);
+    uint32_t offset_start=_offset_index[pos];
+    uint32_t offset_end=(pos!=_offset_index.length()-1)?_offset_index[pos+1]:_offsets.length()-1;
+    vector<uint32_t> offsets;
+    for(uint32_t i=offset_start;i<=offset_end;i++){
+        offsets.push_back(i);
+    }
+    return offsets;
 }
 
 bool PostingList::contain_doc(const uint32_t id) const
